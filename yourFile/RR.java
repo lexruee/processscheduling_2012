932b2a371ddf888186568fc5e9222f659d4ef80b
Alexander Rueedlinger <a.rueedlinger@gmail.com>
2012-04-14 01:11:27 +0200

    copy FCFS as skeleton for RR

--- yourFile/RR.java
+++ yourFile/RR.java
@@ -1,44 +1,136 @@
-
 /* import */
+import java.io.PrintStream;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 import pssimulator.Simulator;
 import pssimulator.SimulatorStatistics;
 
-
 public class RR implements pssimulator.Kernel {
 
-    /* members */
+    public Queue<Process> readyQueue = new LinkedList<Process>();
+    public List<Process> terminatedProcess = new LinkedList<Process>();
+    public Map<String, IODevice> devices = new HashMap<String, IODevice>();
+    public Process idleProcess = new Process(null, "Idle", 0);
+    public Process running = idleProcess;
+
+    // accounting information
+    private int savesCount;
+    private long turnArroundTime;
+    private long waitingMeanTime;
+    private int ps = 0;
+
+    private PrintStream out = System.out;
 
     /* methods */
-
     public void systemCallInitIODevice(String deviceID, Simulator simulator) {
+	out.println("System Call " + deviceID);
+	this.devices.put(deviceID, new IODevice(deviceID));
     }
 
-    public void systemCallProcessCreation(String processID, long timer, Simulator simulator) {
+    /**
+     * a new process is created
+     */
+    public void systemCallProcessCreation(String processId, long timer,
+	    Simulator simulator) {
+	// create a new process
+	Process p = new Process(simulator, processId, timer);
+	out.println("Offer: " + p);
+
+	// add this process to the ready queue
+	this.readyQueue.offer(p);
 
     }
 
-    public void systemCallIORequest(String deviceID, long timer, Simulator simulator) {
+    /**
+     * a user process makes a system call
+     */
+    public void systemCallIORequest(String deviceID, long timer,
+	    Simulator simulator) {
+	out.println("systemCallIORequest pid: " + this.running + ", device: "
+		+ deviceID);
+	// add process to the io waiting queue
+	Process p = this.running;
+	IODevice device = this.devices.get(deviceID);
+	device.offer(p);
+
+	this.running = this.idleProcess;
+
+	// current running process is place into a waiting queue, save cpu
+	// registers
+	// count context switches
+	this.savesCount++;
     }
 
+    /**
+     * user process terminates
+     */
     public void systemCallProcessTermination(long timer, Simulator simulator) {
-
+	Process p = this.running;
+	p.setTerminationTime(timer);
+	this.terminatedProcess.add(p);
+	this.running = this.idleProcess;
+	out.println("Terminate pid: " + this.running + "TAT "
+		+ this.running.getCompletionTime());
     }
 
-    public void interruptIODevice(String deviceID, long timer, Simulator simulator) {
+    /**
+     * an io device interrupt occurs
+     */
+    public void interruptIODevice(String deviceID, long timer,
+	    Simulator simulator) {
+	out.println("interruptIODevice " + deviceID + ", running pid: "
+		+ this.running);
+
+	// get the process from the io waiting queue and it put back into the
+	// ready queue
+	IODevice device = this.devices.get(deviceID);
+	Process p = device.poll();
+	this.readyQueue.offer(p);
+
+	p.startWaitingTimer(timer);
     }
 
     public void interruptPreemption(long timer, Simulator simulator) {
+	// nothing to do!
     }
 
-    public String running(long timer, Simulator simulator) {
-    	return "";
-    	
+    /**
+     * scheduling user processes
+     */
+    public String running(long timer, Simulator sim) {
+	sim.queryOverallTime(this.running.getId());
+
+	// if system is idle then poll the next process from the ready queue
+	if (this.running.equals(idleProcess)) {
+	    if (!this.readyQueue.isEmpty()) {
+		Process p = this.readyQueue.poll();
+		p.finishWatingTimer(timer);
+		this.running = p;
+	    }
+	}
+
+	out.println("running pid: " + this.running + ", WT: "
+		+ this.running.getWaitingTime() + ", current timer: " + timer);
+
+	return this.running.getId();
     }
 
     public void terminate(long timer, SimulatorStatistics sim) {
-    	
-   }
+
+	for (Process p : this.terminatedProcess) {
+	    this.waitingMeanTime += p.getWaitingTime();
+	    this.turnArroundTime += p.getCompletionTime();
+	}
+	this.waitingMeanTime /= this.terminatedProcess.size();
+	this.turnArroundTime /= this.terminatedProcess.size();
+
+	sim.formatStatistics(System.out, sim.getSystemTime(),
+		sim.getUserTime(), sim.getIdleTime(),
+		sim.getSystemCallsCount(), this.savesCount,
+		this.waitingMeanTime, this.turnArroundTime);
+    }
 }
